53a8a238f8d997fc5aaaf0df6c39502df658251c
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-01-14 14:25:47 +0100

    Installe le front

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+read -p "Attention, ce script installe les dépendances dans tout le système. Il n'est donc pas conseillé de l'utiliser sur votre propre machine. Continuer ? [O/n]" yn
+if [ "$yn" == "n" -o "$yn" == "N" ]
+then
+  exit
+fi
+
 # Cloud9
 
 sudo locale-gen fr_FR.UTF-8
 
 # Back-end
 
-echo "Y" | sudo apt-get install git python-dev python-setuptools libxml2-dev python-lxml libxslt-dev libz-dev python-sqlparse libjpeg8 libjpeg8-dev libfreetype6 libfreetype6-dev libffi-dev python-pip python-tox
+sudo apt-get -y install git python-dev python-setuptools libxml2-dev python-lxml libxslt-dev libz-dev python-sqlparse libjpeg8 libjpeg8-dev libfreetype6 libfreetype6-dev libffi-dev python-pip python-tox
 
 # Front-end
 
-echo "Y" | sudo apt-get install nodejs npm nodejs-legacy
+sudo apt-get -y install nodejs npm nodejs-legacy
 sudo npm install -g npm
 
 # Repo
@@ -27,3 +33,9 @@ sudo pip install --upgrade -r requirements-dev.txt
 sudo CFLAGS="-O0" pip install --upgrade lxml==3.4.4
 
 # Libs front
+cp package.json package.back.json
+cat package.back.json | grep -v "gulp-imagemin" > package.json
+npm install
+rm package.json
+mv package.back.json package.json
+sudo reboot

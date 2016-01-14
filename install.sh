6932f1a9237a67e8b79e5c52964450c0dbd23cd5
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-01-14 22:42:09 +0100

    Ajout de logs

--- install.sh
+++ install.sh
@@ -32,14 +32,21 @@ git remote add upstream https://github.com/zestedesavoir/zds-site
 
 # Libs back
 
-sudo cat requirements.txt | grep -v "#" | grep -v "lxml" | xargs pip install --upgrade
-sudo pip install --upgrade -r requirements-dev.txt
-sudo CFLAGS="-O0" pip install --upgrade lxml==3.4.4
+touch requirements.log
+touch requirements-dev.log
+touch lxml.log
+
+sudo cat requirements.txt | grep -v "#" | grep -v "lxml" | xargs pip install --upgrade > requirements.log
+sudo pip install --upgrade -r requirements-dev.txt > requirements-dev.log
+sudo CFLAGS="-O0" pip install --upgrade lxml==3.4.4 > lxml.log
 
 # Libs front
+
+touch npm.log
+
 cp package.json package.back.json
 cat package.back.json | grep -v "gulp-imagemin" > package.json
-npm install
+npm install > npm.log
 rm package.json
 mv package.back.json package.json
 sudo reboot
